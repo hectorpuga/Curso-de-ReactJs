5747e69fef46ec0c4956684518e0a57914f22e4e
Hector Puga <pugagarciah@gmail.com>
2024-03-11 05:48:55 -0600

    Seccion 6 : 75

---
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -0,0 +1,13 @@
+
+export const GifExpertApp = () => {
+    return (
+        <>
+        {/**Titulo */}
+            <h1>GifExpertApp</h1>
+        {/**Input */}
+        {/**Listado de Gif */}
+        {/**Gif Item */}
+
+        </>
+    )
+}
