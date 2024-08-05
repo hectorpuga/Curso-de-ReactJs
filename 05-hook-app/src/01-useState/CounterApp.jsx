a0fbc779dfda48e1ada6472cc9da3dc8fb9694fb
Hector Pugam <pugagarciah@gmail.com>
2024-08-05 00:29:58 -0600

    Sección 9 : 118

---
+++ 05-hook-app/src/01-useState/CounterApp.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export const CounterApp = () => {
+  const [{ counter1, counter2, counter3 }, setState] = useState({
+    counter1: 10,
+    counter2: 20,
+    counter3: 30,
+  });
+  return (
+    <>
+      <h1>Counter1:{counter1} </h1>
+      <h1>Counter2:{counter2} </h1>
+      <h1>Counter3:{counter3} </h1>
+      <hr />
+      <button
+        className="btn"
+        onClick={() => {
+          setState((data)=>({...data,counter1:counter1+1}));
+        }}
+      >
+        +1
+      </button>
+    </>
+  );
+};
