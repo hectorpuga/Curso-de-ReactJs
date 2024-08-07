f726a541a034524f276c7aef559c4180002fe0d5
Hector Puga <hector.puga@enginetsystems.com>
2024-08-07 17:55:52 -0600

    Sección 10: 141

---
+++ 05-hook-app/src/08-useReducer/intro-reducer.js
@@ -0,0 +1,13 @@
+
+
+const initialState=[{
+
+    id:1,
+    todo:'Recolectar la piedra del Alma',
+    done:false
+
+}];
+
+const miFuncion=()=>{
+    
+}
--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -5,12 +5,15 @@ import './index.css'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import SimpleForm from './02-useEffect/SimpleForm'
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHook'
-import { CallbackHook } from './06-memos/CallbackHook'
-import { Padre } from './07-tarea-memo/Padre'
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHook'
+// import { CallbackHook } from './06-memos/CallbackHook'
+// import { Padre } from './07-tarea-memo/Padre'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Padre />
-  </React.StrictMode>,
-)
+import './08-useReducer/intro-reducer'
+
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <Padre />
+//   </React.StrictMode>,
+// )
